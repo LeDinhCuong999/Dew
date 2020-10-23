2aeb4d65e103e5e1d1f3ace334db6c065f843f89
LeDinhCuong999 <73331487+LeDinhCuong999@users.noreply.github.com>
2020-10-23 14:29:02 +0700

    Add files via upload

---
+++ Lab01/VD2-Bai3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{ 
+   int a, b, c, sum;
+   
+   printf("\nEnter any three numbers: ");
+   scanf("%d %d %d, &a, &b, &c"); 
+   
+   sum = a + b + c;
+   
+   printf("\n Sum = %d", sum); 
+}
