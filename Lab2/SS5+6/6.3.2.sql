227cdd286f7af826238238985a06393824a93fb5
LeDinhCuong999 <73331487+LeDinhCuong999@users.noreply.github.com>
2021-01-19 20:52:36 +0700

    Add files via upload

---
+++ Lab2/SS5+6/6.3.2.sql
@@ -0,0 +1,17 @@
+ALTER DATABASE database_name
+
+{
+
+<add_or_modify_files>
+
+| <add_or_modify_filegroups>
+
+| <set_database_options>
+
+| MODIFY NAME = new_database_name
+
+| COLLATE collation_name
+
+}
+
+[;]
