ad4f8b2fe389def9fd2d49040b98b6507070575c
LeDinhCuong999 <73331487+LeDinhCuong999@users.noreply.github.com>
2021-05-21 15:47:00 +0700

    Add files via upload

--- practice2/studentTable.sql
+++ practice2/studentTable.sql
@@ -1,11 +1,11 @@
 create database if not exists school;
 use school;
-create table student(
-    StudentID varchar(10) primary key not null,
-    StudentName varchar(50) not null,
-    Address varchar(50),
-    Phone varchar(10)
+create table Students(
+    studentId varchar(10) primary key not null,
+    studentName varchar(50) not null,
+    address varchar(50),
+    phone inbt(10)
 );
-insert into student values
-    (A123,"Le Dinh Cuong","Hai Duong", 0356623270),
-    (A456, "Nguyen Minh Hiep","Ha Nam",0359794266);
+insert into Students values
+    ('A02150','Le Dinh Cuong','Hai Duong', 0356623270),
+    ('B10230','Nguyen Minh Hiep','Ha Nam',0359794266);
