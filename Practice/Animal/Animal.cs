cb7951eb24ef25d5853814c45e467ab8206e73ff
LeDinhCuong999 <73331487+LeDinhCuong999@users.noreply.github.com>
2021-07-21 15:04:46 +0700

    Practice

---
+++ Practice/Animal/Animal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Animal
+{
+    class Animal
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+
+        public void SetMe(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Name: {Name}, Weight {Weight}");
+        }
+    }
+
+    class Lion: Animal
+    {
+        public Lion()
+        {
+            Name = "Lion";
+            Weight = 350;
+        }
+    }
+
+    class Tiger: Animal
+    {
+        public Tiger()
+        {
+            Name = "Tiger";
+            Weight = 300;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tiger tiger = new Tiger();
+            Console.WriteLine();
+            Lion lion = new Lion();
+
+            tiger.Show();
+            lion.Show();
+        }
+    }
+}
