73fb62b835dcb3bf31562a96268b71e0056e6e3f
LeDinhCuong999 <73331487+LeDinhCuong999@users.noreply.github.com>
2021-09-10 13:03:47 +0700

    done

--- NoiThat/resources/views/noithat (1).sql
+++ NoiThat/resources/views/noithat (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 08, 2021 at 12:16 PM
+-- Generation Time: Sep 10, 2021 at 08:01 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -31,18 +31,22 @@ CREATE TABLE `noithat` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` int(11) NOT NULL,
-  `image` text COLLATE utf8mb4_unicode_ci NOT NULL
+  `image` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `noithat`
 --
 
-INSERT INTO `noithat` (`id`, `name`, `price`, `image`) VALUES
-(5, 'giuong ngu', 923, 'giuong ngu.jpg'),
-(6, 'tu quan ao', 723, 'tu quan ao.jpg'),
-(7, 'ghe sofa', 896, 'ghe sofa.jpg'),
-(8, 'tu tivi', 623, 'tu tivi.jpg');
+INSERT INTO `noithat` (`id`, `name`, `price`, `image`, `created_at`, `updated_at`) VALUES
+(1, 'giuong ngu', 923, 'giuong ngu.jpg', NULL, NULL),
+(2, 'tu quan ao', 723, 'tu quan ao.jpg', NULL, NULL),
+(3, 'ghe sofa', 896, 'ghe sofa.jpg', NULL, NULL),
+(4, 'tu tivi', 623, 'tu tivi.jpg', NULL, NULL),
+(5, 'Dew', 345436, 'elnino4.jpg', '2021-09-09 22:51:49', '2021-09-09 22:51:49'),
+(6, 'C.Ronaldo sv', 23523532, 'bảnh.jpg', '2021-09-09 22:57:54', '2021-09-09 22:57:54');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +66,7 @@ ALTER TABLE `noithat`
 -- AUTO_INCREMENT for table `noithat`
 --
 ALTER TABLE `noithat`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
